1f2e67f444cea4ec09c67491c90548d72148ad1f
musou1500 <musou1500@gmail.com>
2019-06-28 07:02:49 +0900

    remove unnecessary definitions

--- parse.c
+++ parse.c
@@ -3,42 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  Lexer *lexer;
-  Vec *nodes;
-  int pos;
-  char *error;
-} Parser;
-
-enum NodeType {
-  ND_TYPE_DECL,
-  ND_FN_CALL,
-  ND_VAR_DECL,
-};
-
-typedef struct {
-  char *name;
-} TypeDecl;
-
-typedef struct {
-  char *name;
-} FnCall;
-
-typedef struct {
-  char *name;
-  char *ident;
-} VarDecl;
-
-typedef struct {
-  int type;
-
-  union {
-    TypeDecl *type_decl;
-    FnCall *fn_call;
-    VarDecl *var_decl;
-  };
-} Node;
-
 TypeDecl *new_type_decl(char *name) {
   TypeDecl *type_decl = (TypeDecl *)malloc(sizeof(TypeDecl));
   type_decl->name = name;
